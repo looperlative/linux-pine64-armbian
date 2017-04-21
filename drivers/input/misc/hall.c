f1989095b7e24e36aa1d0461d4e3c0648c344aa9
Kamil Trzcinski <ayufan@ayufan.eu>
2017-04-21 23:28:06 +0200

    HALL_REPORT_KEY on Android, HALL_REPORT_SW elsewhere

--- drivers/input/misc/hall.c
+++ drivers/input/misc/hall.c
@@ -53,9 +53,14 @@ static int debug_level = DEBUG_DEBUG;
 #define HALL_OPEN 0
 #define HALL_CLOSE 1
 #define HALL_UNKNOW 2
+
+#ifdef CONFIG_ANDROID_BINDER_IPC
 #define HALL_REPORT_KEY //report key
-//#define HALL_REPORT_SW //report switch
-//#define HALL_STATE_LOCK
+#else                   // CONFIG_ANDROID_BINDER_IPC
+#define HALL_REPORT_SW //report switch
+#endif                 // CONFIG_ANDROID_BINDER_IPC
+
+// #define HALL_STATE_LOCK
 
 struct hall_data {
     int gpio;
